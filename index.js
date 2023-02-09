6ba677ff55f19766f15f4a85f6dfc3e8b812e9a3
dmira1 <darryl.pmira@gmail.com>
2023-02-08 20:20:33 -0700

    unit-converter fixed and complete

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 let length = document.getElementById("length-output");
 let volume = document.getElementById("volume-output");
 let mass = document.getElementById("mass-output");
-let inputValue = document.getElementById("input-box").value;
+let inputValue = document.getElementById("input-box");
 let inputBtn = document.getElementById("input-btn");
+let outputError = document.getElementById("output-error");
 
-inputBtn.addEventListener("click", function() { 
-  if(inputValue != ""){
+inputBtn.addEventListener("click", function() {
+  console.log(typeof inputValue.value);
+  if (inputValue.value > 0) {
+    outputError.innerHTML = "";
     length.innerHTML = `
-      ${inputValue} meters = ${(inputValue* 3.281).toFixed(3)} feet | ${inputValue} feet = ${(inputValue / 0.305).toFixed(3)} meters
+    ${inputValue.value} meters = ${(inputValue.value*3.28084).toFixed(3)} feet | ${inputValue.value} feet = ${(inputValue.value * 0.3048).toFixed(3)} meters
     `;
     volume.innerHTML = `
-      ${inputValue} liters = ${(inputValue* 0.264).toFixed(3)} gallons | ${inputValue} gallons = ${(inputValue * 3.785).toFixed(3)} liters
+      ${inputValue.value} liters = ${(inputValue.value* 0.264172).toFixed(3)} gallons | ${inputValue.value} gallons = ${(inputValue.value * 3.78541).toFixed(3)} liters
     `;
     mass.innerHTML = `
-      ${inputValue} kilos = ${(inputValue* 2.204).toFixed(3)} pounds | ${inputValue} pounds = ${(inputValue * 0.454).toFixed(3)} kilos
+      ${inputValue.value} kilos = ${(inputValue.value* 2.20462).toFixed(3)} pounds | ${inputValue.value} pounds = ${(inputValue.value * 0.453592).toFixed(3)} kilos
     `;
+  } else {
+    outputError.innerHTML = "<h2>Error occured. Invalid format. Please only use numbers greater than zero.</h2>";
   }
 })
